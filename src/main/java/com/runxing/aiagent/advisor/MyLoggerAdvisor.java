ad4b5cffaa58bdf8f0c220c03f1976a8654d62b7
Runxing Han <2284345234@qq.com>
2025-07-12 19:30:58 +0800

    feat(ai-agent): 创建 AI恋爱专家项目
    - 新增 Spring Boot 项目结构和基本配置
    - 实现恋爱心理咨询应用的核心功能
    - 添加单元测试和 API 文档配置
    - 设置 Git忽略文件规则

---
+++ src/main/java/com/runxing/aiagent/advisor/MyLoggerAdvisor.java
@@ -0,0 +1,46 @@
+package com.runxing.aiagent.advisor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.advisor.api.*;
+import org.springframework.ai.chat.model.MessageAggregator;
+import reactor.core.publisher.Flux;
+/**
+ * 自定义日志 Advisor
+ * 打印 info 级别日志、只输出单次用户提示词和 AI 回复的文本
+ */
+@Slf4j
+public class MyLoggerAdvisor implements CallAroundAdvisor, StreamAroundAdvisor {
+
+    @Override
+    public AdvisedResponse aroundCall(AdvisedRequest advisedRequest, CallAroundAdvisorChain chain) {
+        advisedRequest = this.before(advisedRequest);
+        AdvisedResponse advisedResponse =chain.nextAroundCall(advisedRequest);
+        this.observeAfter(advisedResponse);
+        return advisedResponse;
+    }
+
+    @Override
+    public Flux<AdvisedResponse> aroundStream(AdvisedRequest advisedRequest, StreamAroundAdvisorChain chain) {
+        advisedRequest = this.before(advisedRequest);
+        Flux<AdvisedResponse> advisedResponses = chain.nextAroundStream(advisedRequest);
+        return (new MessageAggregator()).aggregateAdvisedResponse(advisedResponses, this::observeAfter);
+    }
+
+    @Override
+    public String getName() {
+        return this.getClass().getSimpleName();
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+
+    private AdvisedRequest before(AdvisedRequest advisedRequest) {
+       log.info("AI Request:{}",advisedRequest.userText());
+        return advisedRequest;
+    }
+    private void observeAfter(AdvisedResponse advisedResponse){
+        log.info("AI Response:{}",advisedResponse.response().getResult().getOutput().getText());
+    }
+}
